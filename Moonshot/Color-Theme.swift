5a0a0f11a485d6a72b12040f92d69f47e4a123aa
DARASKdot <102633287+DARASKdot@users.noreply.github.com>
2023-06-27 14:31:58 +0900

    Formatting our mission view

---
+++ Moonshot/Color-Theme.swift
@@ -0,0 +1,8 @@
+//
+//  Color-Theme.swift
+//  Moonshot
+//
+//  Created by Hiroshi Inoue on R 5/06/27.
+//
+
+import Foundation
--- Moonshot/ContentView.swift
+++ Moonshot/ContentView.swift
@@ -7,7 +7,41 @@ struct ContentView: View {
     let missions: [Mission] = Bundle.main.decode("missions.json")
     
     var body: some View {
-        Text("\(astronauts.count)")
+        NavigationView {
+            ScrollView {
+                LazyVGrid(columns: columns) {
+                    ForEach(missions) { mission in
+                        NavigationLink {
+                            Text("Detail view")
+                        } label: {
+                            VStack {
+                                Image(mission.image)
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: 100, height: 100)
+                                    .padding()
+                                
+                                VStack {
+                                    Text(mission.displayName)
+                                        .font(.headline)
+                                    Text(mission.formattedLaunchDate)
+                                        .font(.caption)
+                                }
+                                .padding(.vertical)
+                                .frame(maxWidth: .infinity)
+                                .background(.lightBackground)
+                            }
+                        }
+                        .clipShape(RoundedRectangle(cornerRadius: 10))
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 10)
+                                .stroke(.lightBackground)
+                        )
+                    }
+                }
+            }
+            .navigationTitle("Moonshot")
+        }
     }
 }
 
